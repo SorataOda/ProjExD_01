4a48bb0f07c145870284c0cf4d9adc98ea83d248
Sorata <c0a2204089@edu.teu.ac.jp>
2023-06-13 15:45:24 +0900

    練習２(訂正)

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
-    bg_img = pg,image.load("ex01-20230613/fig/3.png")
+    kk_img = pg.image.load("ex01-20230613/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
